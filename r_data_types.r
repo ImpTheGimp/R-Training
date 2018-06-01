22fca440996cfbd72e703ffb4bb69cf3e4381b4e
ImpTheGimp <bruce.haskin.vt@gmail.com>
2018-06-01 15:40:30 -0400

    More stuff

--- r_data_types.r
+++ r_data_types.r
@@ -29,4 +29,36 @@ typeof(a)
 
 q1 <- T
 typeof(q1)
+q2 <- FALSE
+typeof(q2)
+
+# store 10 as a double 
+A <- 10
+
+# store 5 as a double
+B <- 5
+
+# add A + B and store them in C
+C <- A + B
+
+# print the value of "C"
+C
+
+# store some doubles
+var1 <- 2.5
+var2 <- 4
+
+# divide the doubles
+result <- var1 / var2
+result
+
+# square root of a variable
+answer <- sqrt(var2)
+answer
+
+# working with strings
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
 
---
+++ r_logical_operators.r
@@ -0,0 +1,24 @@
+# logical operators
+# TRUE T
+# FALSE F
+
+4 < 5
+10 > 100
+4 == 5
+
+# == equal to
+# != not equal to
+# <
+# >
+# <=
+# >=
+# !
+# |
+# &
+# isTRUE(x)
+
+result <- 4 < 5
+typeof(result)
+
+isTRUE(result)
+
