619718cdaeaf8e143875a89a8e9f3a0c3a666238
ImpTheGimp <bruce.haskin.vt@gmail.com>
2018-06-01 16:34:41 -0400

    did some more

---
+++ r_control_structures.r
@@ -0,0 +1,17 @@
+# random number
+x <- rnorm(1)
+
+
+if(x > 1){
+  answer <- "Greater than 1"
+  
+} 
+
+if(x < 1)
+{
+  answer <- "Less than 1"
+}
+
+# scrub a variable
+
+rm(answer)
--- r_loops.r
+++ r_loops.r
@@ -1,3 +1,8 @@
 while(FALSE){
   print("hello")
 }
+
+# 1:5 is a range 1-5 
+for(i in 1:5){
+  print("Hello R")
+}
